3f7deb0ac79fbab22667038b1d2a7ff3d400251a
sonji406 <wkaqh24@gmail.com>
2023-09-11 16:27:04 +0900

    Set: app.js 미들웨어 정리 및 eslint 수정

---
+++ src/loaders/errorHandlers.js
@@ -0,0 +1,14 @@
+const createError = require('http-errors');
+
+module.exports = (app) => {
+  app.use((req, res, next) => {
+    next(createError(404));
+  });
+
+  app.use((err, req, res) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.send(err.message || 'Internal Server Error');
+  });
+};
