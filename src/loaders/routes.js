2294980fe3d6350dc4aaa92691b842f8448848c6
sonji406 <wkaqh24@gmail.com>
2023-09-13 07:54:04 +0900

    Feat: routes 추가

--- src/loaders/routes.js
+++ src/loaders/routes.js
@@ -1,9 +1,13 @@
 const userRouter = require('../routes/user');
 const checkUserByEmailRouter = require('../routes/checkUserByEmail');
 const loginRouter = require('../routes/login');
+const refreshTokenRouter = require('../routes/refresh-token');
+const logoutRouter = require('../routes/logout');
 
 module.exports = (app) => {
-  app.use('/api/user', userRouter);
   app.use('/api/userCheck', checkUserByEmailRouter);
+  app.use('/api/user', userRouter);
   app.use('/api/login', loginRouter);
+  app.use('/api/logout', logoutRouter);
+  app.use('/api/refresh-token', refreshTokenRouter);
 };
