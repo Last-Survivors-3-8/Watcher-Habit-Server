82e45680715d0a92b56be8d90d72fcad66c2570a
sonji406 <wkaqh24@gmail.com>
2023-09-13 07:55:46 +0900

    Feat: 토큰 검증 미들웨어

---
+++ src/middlewares/verifyToken.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const ERRORS = require('../utils/ERRORS');
+const handleError = require('../utils/handleError');
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.Authorization;
+
+  if (!authHeader) {
+    return handleError(res, ERRORS.NO_TOKEN_PROVIDED);
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return handleError(res, ERRORS.TOKEN_INVALID_OR_EXPIRED);
+    }
+
+    req.userId = decoded.userId;
+    next();
+
+    return null;
+  });
+
+  return null;
+};
+
+module.exports = verifyToken;
