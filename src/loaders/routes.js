d7bb4d43b9752e0be230d912e0bb039aae523771
sonji406 <wkaqh24@gmail.com>
2023-09-14 13:04:24 +0900

    Chore: 코드 정리

--- src/loaders/routes.js
+++ src/loaders/routes.js
@@ -1,11 +1,9 @@
 const authRouter = require('../routes/auth');
 const userRouter = require('../routes/user');
 const habitRouter = require('../routes/habit');
-const refreshTokenRouter = require('../routes/refresh-token');
 
 module.exports = (app) => {
   app.use('/api/auth', authRouter);
   app.use('/api/user', userRouter);
   app.use('/api/habit', habitRouter);
-  app.use('/api/refresh-token', refreshTokenRouter);
 };
--- src/utils/handleError.js
+++
@@ -1,9 +0,0 @@
-const handleError = (res, error, data = null) => {
-  const responseBody = { error: error.MESSAGE };
-  if (data) {
-    responseBody.data = data;
-  }
-  res.status(error.STATUS_CODE).json(responseBody);
-};
-
-module.exports = handleError;
