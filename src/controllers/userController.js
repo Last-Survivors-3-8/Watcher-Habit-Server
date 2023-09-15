ef87ff20e47a8e8cab69a5cd20845bb6c9c0ba6a
Josh5519 <AcForCompany@gmail.com>
2023-09-15 15:45:28 +0900

    Refactor: 유저 일별 습관 목록 조회 로직 수정

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,5 @@
 const { ERRORS } = require('../lib/ERRORS');
 const handleError = require('../lib/handleError');
-const Habit = require('../models/Habit');
 const User = require('../models/User');
 const userService = require('../services/userService');
 const dateToDay = require('../utils/dateToDay');
@@ -26,27 +25,27 @@ const getUserCheck = async (req, res, next) => {
 const getUserDailyHabitList = async (req, res, next) => {
   const { date } = req.query;
   const { nickname } = req.params;
-  const dailyHabitList = { nickname: [] };
   const currentDay = dateToDay(date);
 
   try {
-    const user = await User.findOne({ nickname }).lean().exec();
+    const user = await User.findOne({ nickname })
+      .populate({
+        path: 'habits',
+        match: {
+          doDay: { $in: [currentDay] },
+          habitStartDate: { $lte: date },
+          habitEndDate: { $gte: date },
+        },
+        select: '_id habitTitle startTime endTime',
+      })
+      .lean()
+      .exec();
 
     if (!user) {
       return handleError(res, ERRORS.USER_NOT_FOUND);
     }
 
-    dailyHabitList.nickname = await Habit.find(
-      {
-        creator: user._id,
-        doDay: { $in: [currentDay] },
-        habitStartDate: { $lte: date },
-        habitEndDate: { $gte: date },
-      },
-      '_id habitTitle startTime endTime',
-    );
-
-    return res.status(200).json({ data: dailyHabitList });
+    return res.status(200).json({ data: { nickname: user.habits } });
   } catch (error) {
     return next(error);
   }
