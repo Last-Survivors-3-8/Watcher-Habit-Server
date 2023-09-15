4bfe97a54089aed291d48d2bc7f1ed2982991211
Josh5519 <AcForCompany@gmail.com>
2023-09-15 13:13:51 +0900

    Fix:  습관 생성/삭제 api 호출시 연관된 스키마에 습관 id 추가/삭제

--- src/services/habitService.js
+++ src/services/habitService.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const Habit = require('../models/Habit');
 const User = require('../models/User');
 const Group = require('../models/Group');
@@ -13,14 +14,49 @@ const checkGroupExists = (groupId) => Group.exists({ _id: groupId });
 
 const createNewHabit = async (habitData) => {
   const habit = new Habit(habitData);
+  const { creator: userId, sharedGroup: groupId } = habitData;
+
   await habit.save();
+
+  await User.findOneAndUpdate(
+    { _id: userId },
+    { $push: { habits: habit._id } },
+  );
+
+  if (groupId) {
+    await Group.findOneAndUpdate(
+      { _id: groupId },
+      { $push: { habits: habit._id } },
+    );
+  }
+
   return habit;
 };
 
 const updateExistingHabit = (habitId, updatedFields) =>
   Habit.findByIdAndUpdate(habitId, updatedFields, { new: true }).lean().exec();
 
-const deleteHabitById = (habitId) => Habit.findByIdAndDelete(habitId).exec();
+const deleteHabitById = async (habitId) => {
+  const { creator: userId, sharedGroup: groupId } = await getHabitById(habitId);
+
+  const result = await Habit.findByIdAndDelete(habitId).exec();
+
+  const objectHabitId = new mongoose.Types.ObjectId(habitId);
+
+  await User.findOneAndUpdate(
+    { _id: userId },
+    { $pull: { habits: objectHabitId } },
+  );
+
+  if (groupId) {
+    await Group.findOneAndUpdate(
+      { _id: groupId },
+      { $pull: { habits: objectHabitId } },
+    );
+  }
+
+  return result;
+};
 
 module.exports = {
   getHabitById,
