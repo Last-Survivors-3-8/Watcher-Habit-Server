7a3b664ac2d7018d5eb66190eaba86e7ae5c9bd5
Josh5519 <AcForCompany@gmail.com>
2023-09-15 21:05:16 +0900

    Chore: 불필요 로그 제거

--- src/lib/getLoginUserId.js
+++ src/lib/getLoginUserId.js
@@ -3,11 +3,9 @@ const { ERRORS } = require('./ERRORS');
 
 const getLoginUserId = (req) => {
   const token = req.cookies.accessToken;
-  console.log(req);
 
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    console.log(decoded);
     return decoded;
   } catch (error) {
     throw new Error(ERRORS.NEED_TOKEN);
