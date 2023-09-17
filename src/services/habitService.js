46bdc3d039d56f9e9c82fa2fada2bd19d54e2923
sonji406 <wkaqh24@gmail.com>
2023-09-17 14:05:33 +0900

    Feat: 필요한 populate 추가

--- src/services/habitService.js
+++ src/services/habitService.js
@@ -3,7 +3,18 @@ const Habit = require('../models/Habit');
 const User = require('../models/User');
 const Group = require('../models/Group');
 
-const getHabitById = (habitId) => Habit.findById(habitId).lean().exec();
+const getHabitById = (habitId) =>
+  Habit.findById(habitId)
+    .populate({
+      path: 'creator',
+      select: '_id nickname',
+    })
+    .populate({
+      path: 'approvals.userId',
+      select: 'profileImageUrl',
+    })
+    .lean()
+    .exec();
 
 const checkUserExists = (userId) => User.exists({ _id: userId });
 
