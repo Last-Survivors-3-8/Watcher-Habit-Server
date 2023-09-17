d560cc3c9b6c8f6fd0c3bdc4d90e568c3d34dad0
sonji406 <wkaqh24@gmail.com>
2023-09-17 14:04:58 +0900

    Feat: 이미지 업로드 API

--- src/routes/habit.js
+++ src/routes/habit.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const commonErrorHandler = require('../middlewares/commonErrorHandler');
+const multer = require('multer');
+const uploadImage = require('../services/aws/s3');
 const validateHabit = require('../middlewares/validateHabit');
 const habitController = require('../controllers/habitController');
 const validateMiddleware = require('../middlewares/validateMiddleware');
+const commonErrorHandler = require('../middlewares/commonErrorHandler');
 
 const router = express.Router();
 
@@ -49,6 +51,20 @@ router.delete(
   validateMiddleware,
   habitController.deleteHabit,
 );
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+router.post('/:habitId/image', upload.single('image'), async (req, res) => {
+  try {
+    const { habitId } = req.params;
+    const photoKey = `${Date.now().toString()}-${req.file.originalname}`;
+    const url = await uploadImage(req.file.buffer, photoKey, req.file.mimetype);
+
+    res.status(200).json({ message: '사진 업로드 성공', url });
+  } catch (error) {
+    res.status(500).json({ message: '업로드 에러', error });
+  }
+});
 
 router.use(commonErrorHandler);
 
