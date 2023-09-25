fc3c7c53bcaaa1394d9324a308997ec576301410
sonji406 <wkaqh24@gmail.com>
2023-09-25 14:43:54 +0900

    Feat: cron 일정

--- src/utils/cronJob.js
+++ src/utils/cronJob.js
@@ -3,16 +3,17 @@
 const { CronJob } = require('cron');
 const updateAllHabits = require('../lib/updateHabitStatus/updateAllHabits');
 const dailyHabitsBackup = require('../lib/dailyHabitsBackup/dailyHabitsBackup');
+const updateNotifications = require('../lib/updateNotifications/updateNotifications');
 
-// 습관 상태변경 및 알림 전송
-const job = new CronJob('0 */5 * * * *', () => {
+// 습관 상태 업데이트 및 알림 전송
+const updateAllHabitsBatch = new CronJob('*/5 * * * *', () => {
   const currentTime = new Date().toLocaleString();
-  console.log(`배치 실행 로그 - ${currentTime}`);
+  console.log(`습관 상태 업데이트 배치 실행 로그 - ${currentTime}`);
 
   updateAllHabits();
 });
 
-job.start();
+updateAllHabitsBatch.start();
 
 // 일일 습관 백업
 const habitBackupBatch = new CronJob('0 0 */6 * * *', () => {
@@ -23,3 +24,14 @@ const habitBackupBatch = new CronJob('0 0 */6 * * *', () => {
 });
 
 habitBackupBatch.start();
+
+// 알림 상태 업데이트
+const updateNotificationsBatch = new CronJob('*/15 * * * *', () => {
+  console.log(
+    `알림 상태 업데이트 배치 실행 로그 - ${new Date().toLocaleString()}`,
+  );
+
+  updateNotifications();
+});
+
+updateNotificationsBatch.start();
