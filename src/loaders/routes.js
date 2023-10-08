73469e0cb18fb846aaf46c3c6b06c06dacb48f54
sonji406 <wkaqh24@gmail.com>
2023-10-08 09:48:50 +0900

    Chore: 쿠키 api 제거

--- src/loaders/routes.js
+++ src/loaders/routes.js
@@ -4,7 +4,6 @@ const habitRouter = require('../routes/habit');
 const groupRouter = require('../routes/group');
 const notificationRouter = require('../routes/notification');
 const eventsRouter = require('../routes/events');
-const cookieAPI = require('../routes/cookie');
 
 module.exports = (app) => {
   app.use('/api/auth', authRouter);
@@ -13,5 +12,4 @@ module.exports = (app) => {
   app.use('/api/group', groupRouter);
   app.use('/api/notification', notificationRouter);
   app.use('/events', eventsRouter);
-  app.use('/api', cookieAPI);
 };
