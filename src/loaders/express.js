c846cfabcf357f4e027dca09fe212fbd8830043a
sonji406 <wkaqh24@gmail.com>
2023-10-24 14:18:56 +0900

    Chore: express cors 설정 whitelist 적용

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -4,13 +4,21 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
+const whitelist = [process.env.CLIENT_DOMAIN];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
 module.exports = (app) => {
-  app.use(
-    cors({
-      origin: process.env.CLIENT_DOMAIN,
-      credentials: true,
-    }),
-  );
+  app.use(cors(corsOptions));
 
   app.use(logger('dev'));
   app.use(express.json());
